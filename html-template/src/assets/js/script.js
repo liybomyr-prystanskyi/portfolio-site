d93a2feb7721bec495bad2661bf4618717e517ef
liybomyr-prystanskyi <l.prystansky@gmail.com>
2019-10-04 12:01:13 +0300

    add popus

--- html-template/src/assets/js/script.js
+++ html-template/src/assets/js/script.js
@@ -23,6 +23,27 @@ $(function () {
 
 });
 
+var list = $("#works .item");
+var numToShow = 3;
+var button = $("#loadMore");
+var numInList = list.length;
+list.hide();
+if (numInList > numToShow) {
+  button.show();
+}
+list.slice(0, numToShow).show();
+
+button.click(function(){
+    
+    var showing = list.filter(':visible').length;
+    list.slice(showing - 1, showing + numToShow).fadeIn();
+    var nowShowing = list.filter(':visible').length;
+    if (nowShowing >= numInList) {
+      button.hide();
+    }
+});
+
+
 $("ul li:has(ul)").addClass("has-submenu");
 $("ul li ul").addClass("sub-menu");
 $("ul.dropdown li").hover(function () {
